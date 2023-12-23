b790aeaa4b9f698c1b7779d2e2dbca0dc4819933
Abhishek Singh <abesingh1@gmail.com>
2023-12-23 13:31:32 +0530

    styled components: pseudo selectors, nested rules and media queries

--- styling-react-component/src/components/AuthInputs.jsx
+++ styling-react-component/src/components/AuthInputs.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styled from "styled-components";
+import { styled } from "styled-components";
 
 const ControlContainer = styled.div`
   display: flex;
@@ -28,6 +28,21 @@ const Input = styled.input`
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
   border-color: ${({ $invalid }) => ($invalid ? "#f73f3f" : "")};
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover: {
+    background-color: #f0920e;
+  }
 `;
 
 export default function AuthInputs() {
@@ -79,9 +94,9 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button className="button" onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
