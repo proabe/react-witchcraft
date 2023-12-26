160488878cb92d0723d29a958c22e8c97f1c54c8
Abhishek Singh <abesingh1@gmail.com>
2023-12-26 15:53:29 +0530

    using states

--- react-refs-and-portals/src/components/Player.jsx
+++ react-refs-and-portals/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [playerName, setPlayerName] = useState(null);
+
+  function handleChange(event) {
+    setIsSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setIsSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSubmitted ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleChange} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
